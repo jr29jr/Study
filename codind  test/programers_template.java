a23116bb7dc0dcf938b02a4c03793c6f6ca15e76
jr29jr <jr30jr@naver.com>
2022-03-13 12:37:24 +0900

    Create programers_template.java

---
+++ codind  test/programers_template.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.*;
+
+
+class Main{
+  static StringBuilder sb = new StringBuilder();
+  static FastReader scan = new FastReader();
+  
+  static void input() {
+  }
+  
+  public static void main(String[] args) {
+    input();
+    //Solution 호출(전달인자,리턴은 문제에 알맞게)
+    int answer=new Solution().solution();
+    System.out.println(answer);
+  }
+  
+  static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
