e9ba594d0d924a082e3d66de09b8a3be924fffb8
jr29jr <jr30jr@naver.com>
2022-03-13 12:04:37 +0900

    재귀로 문제를 풀 경우
    입력과 연산 출력을 나눠서 풀자.

---
+++ baekjoon/java_template.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.util.*;
+
+class algorithm_templet{
+  static StringBuilder sb = new StringBuilder();
+  static FastReader scan = new FastReader();
+
+  static void input() {
+  
+  }
+   
+
+  static void rec_func(){
+    
+  }
+  
+  public static void main(String[] args) {
+    input();
+    rec_func();
+    System.out.println(sb.toString());
+  }
+  
+  
+  static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
