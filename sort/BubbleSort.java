7c6500796957b7b4e6aa945815989609e7fb1fd6
jr29jr <jr30jr@naver.com>
2023-01-12 22:14:39 +0900

    오름차순 버블소트 구현
    int[]의 오름차순 구현

--- sort/BubbleSort.java
+++ sort/BubbleSort.java
@@ -1,16 +1,28 @@
-import java.util.*;
+import java.util.Arrays;
 
 public class BubbleSort {
-    //오름차순
-    static void sort(int[] array,int size){
-        for(int i=0;i<size;i++){
-            for(int j=0;j<=size-2-i;j++){
-                if(array[j]>array[j+1]){
-                    int temp=array[j];
-                    array[j]=array[j+1];
-                    array[j+1]=temp;
-                }
+    /** 
+     * 전달인자 int[]을 버블소트로 오름차순 정렬한다.
+     */
+    public static void bubblesort(int[] array,int n){
+        //부분 정렬을 몇번할지 n-1개 해야한다.
+        for(int i=0;i<n-1;i++){
+            //비교해서 정렬할 범위
+            //1번 정렬할때마다 정렬할 수가 줄어든다.
+            for(int j=0;j<n-1-i;j++){
+                if(array[j] > array[j+1])
+                    swap(array,j,j+1);
             }
+            System.out.println(Arrays.toString(array));
         }
     }
+    /**
+     * array의 index1의 값과 index2의 값을 변경
+     */
+    private static void swap(int[] array,int index1,int index2){
+        int temp=array[index1];
+        array[index1]=array[index2];
+        array[index2]=temp;
+    }
+
 }
